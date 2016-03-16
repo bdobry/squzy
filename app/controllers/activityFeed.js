d5051b04f890e36421799bd89d61717032254930
bdobry <bartoszdobry@gmail.com>
2016-03-16 20:42:26 +0100

    navbar 2 elements

--- app/controllers/activityFeed.js
+++ app/controllers/activityFeed.js
@@ -1,2 +1,20 @@
-// Arguments passed into this controller can be accessed off of the `$.args` object directly or:
-var args = $.args;
+var view = Ti.UI.createView({
+
+});
+
+var btn1 = Ti.UI.createButton({
+    title :'M',
+    right : 0,
+    width : 30,
+    height : 30
+});
+var btn2 = Ti.UI.createButton({
+    title :'+',
+    right : 30,
+    width : 30,
+    height : 30
+});
+view.add(btn1);
+view.add(btn2);
+
+$.win2.setRightNavButton(view);
