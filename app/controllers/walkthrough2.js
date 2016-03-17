8fa7b153b528c2b204284564c33a9c5a783b0782
bdobry <bartoszdobry@gmail.com>
2016-03-17 22:28:35 +0100

    Coming back to last progress
    This reverts commit cdfc43fc7cbdbe0116740e5c64ddfd86fe914a71.

--- app/controllers/walkthrough2.js
+++ app/controllers/walkthrough2.js
@@ -5,12 +5,6 @@ var navWindow = Alloy.createController('navbar').getView();
 var newUser = "";
 var newpassword = "";
 
-function goActivity(){
-	navWindow.open();
-	activityFeed.open();
-}
-
-
 function SetLogin()
 {
 	newUser = $.username.value;
