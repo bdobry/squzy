db09e81de1f89efe7caba7de8277098d06bf54ae
bdobry <bartoszdobry@gmail.com>
2016-03-17 17:08:09 +0100

    almost working training screen

--- app/controllers/training.js
+++ app/controllers/training.js
@@ -6,11 +6,71 @@ function goActivity(){
 }
  
 
-function animWidth() {
-	var a = Ti.UI.createAnimation({ 
-    	left: "100%",
-    	duration: 1000
-	});
+var a = Ti.UI.createAnimation({ 
+	left: "100%",
+	duration: 2000
+});
+
+var z = Ti.UI.createAnimation({ 
+	left: 0,
+	duration: 500
+});
+
+var x = Ti.UI.createAnimation({ 
+	left: 0,
+	duration: 4000
+});
+
+var complete = "false";
+var countdown = 10;
+var clickable = "true";
+var broken = "false";
+var buttonText = $.pusher.text;
+
+$.countdown.text = countdown;
+
+$.pusher.addEventListener('touchstart', function(e){
+    if (clickable == "true") {
+	    $.timer.animate(a);
+	    
+	    setTimeout(function(){
+			 if (broken == "false") {
+				countdown -=1;
+				clickable = "false";
+				rest();
+				// alert('Countdown: '+ countdown);
+				$.countdown.text = countdown;
+			} else {
+				broken = "false";
+				complete = "false";
+				clickable = "true";
+			}
+		},2000);
+	} else {
+		setTimeout(function(){
+			clickable = "true"; 
+		},500);
+	}
+
+}); 
+
+$.pusher.addEventListener('touchend', function(e){
+    if (clickable == "true") {
+	    broken = "true";
+	    clickable = "false";
+	    $.timer.animate(z);    	
+    }
+}); 
+
+function rest() {
+	$.timer.animate(x);
+	clickable = "false";
+	buttonText = "relax now...";
 	
-	$.timer.animate(a);
+	setTimeout(function(){
+		clickable = "true"; 
+		completed = "false";
+		broken = "false";
+		buttonText = "press and squeeze";
+		},4000);
 }
