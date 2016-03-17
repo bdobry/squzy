dd3ce22e16f6504ed4767e97b70d969c2095e827
bdobry <bartoszdobry@gmail.com>
2016-03-17 13:28:54 +0100

    init animation Training screen

--- app/controllers/training.js
+++ app/controllers/training.js
@@ -4,3 +4,13 @@ function goActivity(){
 	activity.open();
 	navbar.open();
 }
+ 
+
+function animWidth() {
+	var a = Ti.UI.createAnimation({ 
+    	left: "100%",
+    	duration: 1000
+	});
+	
+	$.timer.animate(a);
+}
